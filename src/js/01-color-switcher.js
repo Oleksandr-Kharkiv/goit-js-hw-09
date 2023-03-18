18136418e1c6cc6b85385337f36ff8fde09a8dc5
Oleksandr-Kharkiv <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-03-18 02:56:50 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+
+let intervalId = null;
+let isActiveColorChange = false;
+
+btnStartEl.addEventListener('click', () => {
+  if (isActiveColorChange) {
+    return;
+  }
+  isActiveColorChange = true;
+  intervalId = setInterval(() => {
+    bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+    console.log('Поточний колір фону:', bodyEl.style.backgroundColor);
+    console.log(intervalId);
+  }, 1000);
+  console.log('Функція зміни кольору ввімкнена:', isActiveColorChange);
+});
+
+btnStopEl.addEventListener('click', () => {
+  clearInterval(intervalId);
+  isActiveColorChange = false;
+  console.warn('Функція зміни кольору ввімкнена:', isActiveColorChange);
+});
