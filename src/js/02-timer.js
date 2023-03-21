d115266406c205fb3314c219d3be02d8ee0596b2
Oleksandr-Kharkiv <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-03-21 21:47:40 +0100

    add task 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,68 +1,80 @@
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 import Notiflix from 'notiflix';
-import "flatpickr/dist/flatpickr.min.css"
+import 'flatpickr/dist/flatpickr.min.css';
 
 const daysEl = document.querySelector('span[data-days]');
 const hoursEl = document.querySelector('span[data-hours]');
 const minutesEl = document.querySelector('span[data-minutes]');
 const secondsEl = document.querySelector('span[data-seconds]');
 const calendarEl = document.querySelector('input#datetime-picker');
+const timerEl = document.querySelector('.timer');
+const colorBlockEl = timerEl.lastElementChild;
 const btnStartEl = document.querySelector('button[data-start]');
 btnStartEl.disabled = true;
 let chosenTime = null;
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        if(new Date() > selectedDates[0]){
-            Notiflix.Notify.failure('Please choose a date in the future');
-            btnStartEl.disabled = true;
-        } else {
-            btnStartEl.disabled = false;
-            chosenTime = selectedDates[0];
-        }
-    },};
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (new Date() > selectedDates[0]) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      btnStartEl.disabled = true;
+    } else {
+      btnStartEl.disabled = false;
+      chosenTime = selectedDates[0];
+    }
+  },
+};
 flatpickr(calendarEl, options);
-    
-btnStartEl.addEventListener('click', timer);
-    
-function timer() {
-    const intervalId = setInterval(() => {
-        btnStartEl.disabled = true;
-        const deltaTime = chosenTime - new Date();
-        tablo(convertMs(deltaTime));
-        if(deltaTime <= 999){
-            Notiflix.Report.success('The end', 'Wait for new starts', '😉');
-            clearInterval(intervalId);
-        }
-    }, 1000)}
 
-function addLeadingZero(value){
-    return String(value).padStart(2, '0');
+btnStartEl.addEventListener('click', timer);
+
+function timer() {
+  const intervalId = setInterval(() => {
+    btnStartEl.disabled = true;
+    const deltaTime = chosenTime - new Date();
+    tablo(convertMs(deltaTime));
+    colorBlockEl.style.backgroundColor = `${getRandomHexColor()}`;
+    if (deltaTime <= 999) {
+      Notiflix.Report.success('The end', 'Wait for new starts', '😉');
+      clearInterval(intervalId);
+      return
+    }
+  }, 1000);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
 }
 
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-    // Remaining days
-    const days = addLeadingZero(Math.floor(ms / day));
-    // Remaining hours
-    const hours = addLeadingZero(Math.floor((ms % day) / hour));
-    // Remaining minutes
-    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-    // Remaining seconds
-    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
-    return { days, hours, minutes, seconds };
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+  return { days, hours, minutes, seconds };
 }
 
-function tablo({ days, hours, minutes, seconds }){
-    daysEl.textContent = `${days}`;
-    hoursEl.textContent = `${hours}`;
-    minutesEl.textContent = `${minutes}`;
-    secondsEl.textContent = `${seconds}`;
+function tablo({ days, hours, minutes, seconds }) {
+  daysEl.textContent = `${days}`;
+  hoursEl.textContent = `${hours}`;
+  minutesEl.textContent = `${minutes}`;
+  secondsEl.textContent = `${seconds}`;
 }
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  let delayNum = Number(delay.value);
+  const stepNum = Number(step.value);
+  const amountNum = Number(amount.value);
+
+  for (let position = 1; position < amountNum + 1; position += 1) {
+    createPromise(position, delayNum)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delayNum += stepNum;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        console.log(`Проміс ${position} відпрацював`);
+        resolve({ position, delay });
+      } else {
+        console.log(`Проміс ${position} провалився`);
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
